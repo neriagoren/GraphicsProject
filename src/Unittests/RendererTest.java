8234350dfa47128451d581efa1a287933b5b7c83
neriagoren <neriatheman@gmail.com>
2021-12-05 18:23:19 +0200

    modified image

--- src/Unittests/RendererTest.java
+++ src/Unittests/RendererTest.java
@@ -24,7 +24,7 @@ class RendererTest {
         Scene scene = new Scene("SHAPES");
         scene.setCamera(new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0)));
         scene.setScreenDistance(100) ;
-        scene.setBackground(new Color(0,0,0));
+        scene.setBackground(new Color(86, 156, 177));
 
 
         scene.addGeometry(new Triangle(new Point3D(0,-300, 200), new Point3D(-125, -150, 200), new Point3D(125,-150,200), Color.YELLOW));
@@ -33,7 +33,7 @@ class RendererTest {
 
         List<Point3D> hexagon = new ArrayList<>();
         List<Point3D> square = new ArrayList<>();
-        List<Point3D> octagon = new ArrayList<>();
+        List<Point3D> pentagon = new ArrayList<>();
 
         square.add(new Point3D(250,-100, 200));
         square.add(new Point3D(250,100, 200));
@@ -48,16 +48,16 @@ class RendererTest {
         hexagon.add(new Point3D(-350,-50, 200));
 
 
-        octagon.add(new Point3D(-0,150, 200));
-        octagon.add(new Point3D(50,335, 200));
-        octagon.add(new Point3D(-50,335, 200));
-        octagon.add(new Point3D(100,225, 200));
-        octagon.add(new Point3D(-100,225, 200));
+        pentagon.add(new Point3D(-0,150, 200));
+        pentagon.add(new Point3D(50,335, 200));
+        pentagon.add(new Point3D(-50,335, 200));
+        pentagon.add(new Point3D(100,225, 200));
+        pentagon.add(new Point3D(-100,225, 200));
 
 
         scene.addGeometry(new Polygon(hexagon, Color.BLUE));
         scene.addGeometry(new Polygon(square, Color.RED));
-        scene.addGeometry(new Polygon(octagon, Color.MAGENTA));
+        scene.addGeometry(new Polygon(pentagon, Color.MAGENTA));
 
 
         ImageWriter imageWriter = new ImageWriter("SHAPES", 500, 500, 500, 500);
